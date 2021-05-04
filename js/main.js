ecccaaf4d4248d29a92ebe39032d7a118c414002
k-k-kei <42371057+k-k-kei@users.noreply.github.com>
2021-05-05 02:18:48 +0900

    ディスプレイ切り替え機能実装

--- js/main.js
+++ js/main.js
@@ -19,9 +19,10 @@ class Post {
 
 // データ保存
 $(".savebtn").on("click", function () {
-    const title = $(".inputbox-title-input").val();
-    const text = $(".input-text-long").val();
+    let title = $(".inputbox-title-input").val();
+    let text = $(".input-text-long").val();
     let input = new Post(title, text);
+    console.log(title, text);
     localStorage.setItem(title, JSON.stringify(input));
     location.reload();
 });
@@ -34,23 +35,27 @@ for (let i = 0; i < localStorage.length; i++) {
     $(".memoblock-contents").append(html).hide().fadeIn(400);
 }
 
+// ランダムな数字を取得
 function random() {
     let len = localStorage.length;
     return Math.floor(Math.random() * len);
 }
 
+// ローカルストレージからランダムでデータを取得
 function shuffle1() {
     let num = random();
     const key = localStorage.key(num);
     return key;
 }
 
+// ローカルストレージからランダムでデータを取得
 function shuffle2() {
     let num = random();
     const key = localStorage.key(num);
     return key;
 }
 
+// ランダムなデータを表示
 $(".shuffle").on("click", function () {
     let key1 = shuffle1();
     let key2 = shuffle2();
@@ -61,4 +66,31 @@ $(".shuffle").on("click", function () {
     $(".memo1-text").html(memo1.text);
     $(".memo2-title").html(memo2.title);
     $(".memo2-text").html(memo2.text);
+});
+
+// モード切り替え1
+$(".mode-btn1").on("click", function () {
+    $(".second-contents").css("display", "none");
+    $(".show").fadeIn();
+});
+
+// モード切り替え2
+$(".mode-btn2").on("click", function () {
+    $(".show").css("display", "none");
+    $(".second-contents").css("display", "block");
+    $(".modal").css("display", "contents");
+});
+
+// モーダルを閉じる
+$(".modal-closebtn").on("click", function () {
+    $(".modal").css("display", "none");
+});
+
+// モーダルを保存
+$(".modal-savebtn").on("click", function () {
+    let title = $(".inputbox-title-input").val();
+    let text = $(".input-text-long").val();
+    $(".static-title").html(title);
+    $(".static-text").html(text);
+    $(".modal").css("display", "none");
 });
