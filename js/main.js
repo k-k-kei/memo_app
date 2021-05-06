006a5e4d1e3d77e30ba488862f1d07bdacd574ac
k-k-kei <42371057+k-k-kei@users.noreply.github.com>
2021-05-06 23:24:02 +0900

    firebaseデータベースを実装

--- js/main.js
+++ js/main.js
@@ -1,95 +1,102 @@
 
-// メモ入力フォームオープン
-$(".new-item").on("click", function () {
-    $(".modal").fadeIn();
-});
+// // メモ入力フォームオープン
+// $(".new-item").on("click", function () {
+//     $(".modal").fadeIn();
+// });
 
-// メモ入力フォームクローズ
-$(".closebtn").on("click", function () {
-    $(".modal").fadeOut();
-});
+// // メモ入力フォームクローズ
+// $(".closebtn").on("click", function () {
+//     $(".modal").fadeOut();
+// });
 
 
-class Post {
-    constructor(title, text) {
-        this.title = title;
-        this.text = text;
-    }
-}
+// // データ保存
+// $(".savebtn").on("click", function () {
+//     let postkey = 0;
+//     let title = $(".inputarea-title-input").val();
+//     let text = $(".input-text-long").val();
+//     let post = {
+//         postkey: postkey,
+//         title: title,
+//         text: text
+//     }
+//     ref.push(post);
+// });
 
-// データ保存
-$(".savebtn").on("click", function () {
-    let title = $(".inputarea-title-input").val();
-    let text = $(".input-text-long").val();
-    let input = new Post(title, text);
-    localStorage.setItem(title, JSON.stringify(input));
-    location.reload();
-});
+// // データを表示
+// // for (let i = 0; i < localStorage.length; i++) {
+// //     const key = localStorage.key(i);
+// //     const value = JSON.parse(localStorage.getItem(key));
+// //     const html = '<div class="textarea-items"><p class="textarea-items-title">' + key + '</p><p class="textarea-items-text">' + value.text + '</p></div>';
+// //     $(".memoblock-contents").append(html).hide().fadeIn(400);
+// // }
 
-// データを表示
-for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    const value = JSON.parse(localStorage.getItem(key));
-    const html = '<div class="textarea-items"><p class="textarea-items-title">' + key + '</p><p class="textarea-items-text">' + value.text + '</p></div>';
-    $(".memoblock-contents").append(html).hide().fadeIn(400);
-}
+// ref.on("child_added", function (data) {
+//     let v = data.val();
+//     // let k = data.key;
+//     const h = '<div class="textarea-items"><p class="textarea-items-title">' + v.title + '</p><p class="textarea-items-text">' + v.text + '</p></div>';
+//     $(".memoblock-contents").append(h);
+//     const messagesArea = document.getElementsByClassName('memoblock-contents');
+//     messagesArea.scrollTop = messagesArea.scrollHeight;
+// });
 
-// ランダムな数字を取得
-function random() {
-    let len = localStorage.length;
-    return Math.floor(Math.random() * len);
-}
+// // ランダムな数字を取得
+// function random() {
+//     let len = localStorage.length;
+//     return Math.floor(Math.random() * len);
+// }
 
-// ローカルストレージからランダムでデータを取得
-function shuffle1() {
-    let num = random();
-    const key = localStorage.key(num);
-    return key;
-}
+// // ローカルストレージからランダムでデータを取得
+// function shuffle1() {
+//     let num = random();
+//     const key = localStorage.key(num);
+//     return key;
+// }
 
-// ローカルストレージからランダムでデータを取得
-function shuffle2() {
-    let num = random();
-    const key = localStorage.key(num);
-    return key;
-}
+// // ローカルストレージからランダムでデータを取得
+// function shuffle2() {
+//     let num = random();
+//     const key = localStorage.key(num);
+//     return key;
+// }
 
-// ランダムなデータを表示
-$(".shuffle").on("click", function () {
-    let key1 = shuffle1();
-    let key2 = shuffle2();
-    const memo1 = JSON.parse(localStorage.getItem(key1));
-    const memo2 = JSON.parse(localStorage.getItem(key2));
-    console.log([memo1, memo2]);
-    $(".memo1-title").html(memo1.title);
-    $(".memo1-text").html(memo1.text);
-    $(".memo2-title").html(memo2.title);
-    $(".memo2-text").html(memo2.text);
-});
+// // ランダムなデータを表示
+// $(".shuffle").on("click", function () {
+//     let key1 = shuffle1();
+//     let key2 = shuffle2();
+//     const memo1 = JSON.parse(localStorage.getItem(key1));
+//     const memo2 = JSON.parse(localStorage.getItem(key2));
+//     console.log([memo1, memo2]);
+//     $(".memo1-title").html(memo1.title);
+//     $(".memo1-text").html(memo1.text);
+//     $(".memo2-title").html(memo2.title);
+//     $(".memo2-text").html(memo2.text);
+// });
 
-// モード切り替え1
-$(".mode-btn1").on("click", function () {
-    $(".second-contents").css("display", "none");
-    $(".show").fadeIn();
-});
+// // モード切り替え1
+// $(".mode-btn1").on("click", function () {
+//     $(".second-contents").css("display", "none");
+//     $(".show").fadeIn();
+// });
 
-// モード切り替え2
-$(".mode-btn2").on("click", function () {
-    $(".show").css("display", "none");
-    $(".second-contents").css("display", "block");
-    $(".modal").css("display", "contents");
-});
+// // モード切り替え2
+// $(".mode-btn2").on("click", function () {
+//     $(".show").css("display", "none");
+//     $(".second-contents").css("display", "block");
+//     $(".modal").css("display", "contents");
+// });
 
-// モーダルを閉じる
-$(".modal-closebtn").on("click", function () {
-    $(".modal").css("display", "none");
-});
+// // モーダルを閉じる
+// $(".modal-closebtn").on("click", function () {
+//     $(".modal").css("display", "none");
+// });
 
-// モーダルを保存
-$(".modal-savebtn").on("click", function () {
-    let title = $(".modal-title-input").val();
-    let text = $(".modal-text-long").val();
-    $(".static-title").html(title);
-    $(".static-text").html(text);
-    $(".modal").css("display", "none");
-});
+// // モーダルを保存
+// $(".modal-savebtn").on("click", function () {
+//     let title = $(".modal-title-input").val();
+//     let text = $(".modal-text-long").val();
+//     $(".static-title").html(title);
+//     $(".static-text").html(text);
+//     $(".modal").css("display", "none");
+// });
+
