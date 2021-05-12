3bed3adbc9a762d104519a082f3a01c73d3f91e4
k-k-kei <42371057+k-k-kei@users.noreply.github.com>
2021-05-12 09:56:35 +0900

    レスポンシブデザイン実装（6割）

--- js/main.js
+++ js/main.js
@@ -8,90 +8,94 @@ $(".closebtn").on("click", function () {
     $(".modal").fadeOut();
 });
 
-// firebaseの設定
-var firebaseConfig = {
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
     apiKey: "AIzaSyAL-R87FbZKeq-v8gZqy7VeKg5iqy8Cv94",
     authDomain: "memoma-f6749.firebaseapp.com",
     projectId: "memoma-f6749",
-    storageBucket: "memoma-f6749.appspot.com",
-    messagingSenderId: "361517209271",
-    appId: "1:361517209271:web:8a69cd1a023f12774d5f32"
-};
+});
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+var db = firebase.firestore();
 
 // ログインユーザーの値を取得
 var unsubscribe = firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         // ログインしていれば中通る
         // ユーザー情報が表示される
-        console.log(user);
-        uname = user.displayName;
-        console.log(uname);
 
-        // firebaseへデータ保存
-        $(".savebtn").on("click", function () {
+        function getData() {
             let postkey = 0;
             let username = user.displayName;
             let userId = user.uid;
             let title = $(".inputarea-title-input").val();
             let text = $(".input-text-long").val();
-            let post = {
+
+            db.collection("ideas").add({
                 postkey: postkey,
                 username: username,
                 userId: userId,
                 title: title,
                 text: text
-            }
-            db.ref().push(post);
+            });
+        }
+
+        function addData() {
+            let postkey = 1;
+            let username = user.displayName;
+            let userId = user.uid;
+            let text = $(".show-answer").val();
+
+            db.collection("idea_seeds").add({
+                postkey: postkey,
+                username: username,
+                userId: userId,
+                text: text
+            });
+        }
+
+        // firebaseへデータ保存
+        $(".savebtn").on("click", function () {
+            getData();
             $(".inputarea-title-input").val("");
             $(".input-text-long").val("");
         });
 
         // firebaseデータの表示
-        db.ref().on("child_added", function (data) {
-            let v = data.val();
-            let p = v.postkey;
-            let t = v.title;
-            let x = v.text;
-            let userId = user.uid;
-            let dataId = v.userId;
-            if (p === 0 && userId === dataId) {
-                sessionStorage.setItem(t, x)
-                const h = '<div class="textarea-items"><p class="textarea-items-title">' + v.title + '</p><p class="textarea-items-text">' + v.text + '</p></div>';
-                $(".memoblock-contents").prepend(h);
-            }
+        db.collection("ideas").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                let v = doc.data();
+                let p = v.postkey;
+                let t = v.title;
+                let x = v.text;
+                let userId = user.uid;
+                let dataId = v.userId;
+                if (p === 0 && userId === dataId) {
+                    sessionStorage.setItem(t, x)
+                    const h = '<div class="textarea-items"><p class="textarea-items-title">' + v.title + '</p><p class="textarea-items-text">' + v.text + '</p></div>';
+                    $(".memoblock-contents").prepend(h);
+                }
+            });
         });
 
-        // 掛け合わせアイデアの保存
+        // // 掛け合わせアイデアの保存
         $(".idea-save").on("click", function () {
-            let postkey = 1;
-            let username = user.displayName;
-            let userId = user.uid;
-            let text = $(".show-answer").val();
-            let post = {
-                postkey: postkey,
-                username: username,
-                userId: userId,
-                text: text
-            }
-            db.ref().push(post);
+            addData();
             $(".show-answer").val('');
         });
 
-        db.ref().on("child_added", function (data) {
-            let v = data.val();
-            let p = v.postkey;
-            let x = v.text;
-            let userId = user.uid;
-            let dataId = v.userId;
-            if (p === 1 && userId === dataId) {
-                const h = '<div class="memolist"><div class="stock"><div class="delete-tag"><img src="image/delete-tag.svg" alt="button" class="tag-image"></div><div class="stock-text">' + x + '</div ></div></div>';
-                $(".idea-board").prepend(h);
-            };
-
+        db.collection("idea_seeds").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                let v = doc.data();
+                let x = v.text;
+                let userId = user.uid;
+                let dataId = v.userId;
+                if (userId === dataId) {
+                    const h = '<div class="memolist"><div class="stock"><div class="delete-tag"><img src="image/delete-tag.svg" alt="button" class="tag-image"></div><div class="stock-text">' + x + '</div ></div></div>';
+                    $(".idea-box").prepend(h);
+                }
+            });
         });
+
     }
     // 登録解除
     unsubscribe();
@@ -128,8 +132,7 @@ $(".shuffle").on("click", function () {
     let val1 = sessionStorage.getItem(memo1);
     let memo2 = shuffle2();
     let val2 = sessionStorage.getItem(memo2);
-    // const memo1 = JSON.parse(sessionStorage.getItem(key1));
-    // const memo2 = JSON.parse(sessionStorage.getItem(key2));
+
     console.log([memo1, memo2]);
     $(".memo1-title").html(memo1);
     $(".memo1-text").html(val1);
